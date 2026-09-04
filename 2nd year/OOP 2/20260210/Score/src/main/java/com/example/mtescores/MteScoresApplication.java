4e9e1376282d9ead07958a3e5ada040e1b338a00
zyoJoef <cadornajoeffrey9@gmail.com>
2026-09-04 12:21:26 +0800

    Add files via upload

---
+++ 2nd year/OOP 2/20260210/Score/src/main/java/com/example/mtescores/MteScoresApplication.java
@@ -0,0 +1,11 @@
+package com.example.mtescores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+
+@SpringBootApplication
+public class MteScoresApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MteScoresApplication.class, args);  
+    }
+}
