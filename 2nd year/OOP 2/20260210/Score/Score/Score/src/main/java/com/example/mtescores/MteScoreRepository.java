787c5865c6bf8bdf0a8fdd4a968c02afe22c413c
zyoJoef <cadornajoeffrey9@gmail.com>
2026-09-04 12:20:29 +0800

    Delete 2nd year/OOP 2/20260210/Score/Score/Score directory

--- 2nd year/OOP 2/20260210/Score/Score/Score/src/main/java/com/example/mtescores/MteScoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mtescores;
-
-import org.springframework.data.jpa.repository.JpaRepository;  
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MteScoreRepository extends JpaRepository<MteScore, Integer> {
-}
